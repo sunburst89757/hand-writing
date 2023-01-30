da85416c280321d09faf7c049d232c05956fd260
sunburst89757 <3520279278@qq.com>
2023-01-30 20:38:55 +0800

    fix:修复监听时上一次监听的事件丢失的bug

--- src/3.eventEmitter.js
+++ src/3.eventEmitter.js
@@ -1,10 +1,21 @@
+/*
+ * @Author: sunburst89757 3520279278@qq.com
+ * @Date: 2022-12-30 22:04:46
+ * @LastEditors: sunburst89757 3520279278@qq.com
+ * @LastEditTime: 2023-01-30 20:37:52
+ * @Description:
+ *
+ * Copyright (c) 2023 by sunburst89757 3520279278@qq.com, All Rights Reserved.
+ */
 //  发布订阅模式
 class EventEmitter {
   constructor() {
     this.queue = {};
   }
   on(name, fn) {
-    this.queue[name] = [];
+    if (this.queue[name] === undefined) {
+      this.queue[name] = [];
+    }
     this.queue[name].push(fn);
   }
   off(name, fn) {
