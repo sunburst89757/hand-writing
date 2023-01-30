2ed3452bc4bf3e4710abc195843a90f9e342869b
sunburst89757 <3520279278@qq.com>
2023-01-30 21:22:24 +0800

    feat:节流

---
+++ src/17.throttle.js
@@ -0,0 +1,21 @@
+/*
+ * @Author: sunburst89757 3520279278@qq.com
+ * @Date: 2023-01-30 21:15:34
+ * @LastEditors: sunburst89757 3520279278@qq.com
+ * @LastEditTime: 2023-01-30 21:20:12
+ * @Description:
+ *
+ * Copyright (c) 2023 by sunburst89757 3520279278@qq.com, All Rights Reserved.
+ */
+function throttle(fn, delay) {
+  let timer;
+  return function (...args) {
+    if (!timer) {
+      timer = setTimeout(() => {
+        fn.call(this, ...args);
+        // 执行完毕后要手动将timer 清0 ，定时器并不会执行完毕后自动将timer清0
+        timer = null;
+      }, delay);
+    }
+  };
+}
